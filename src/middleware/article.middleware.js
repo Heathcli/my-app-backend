dc2ca255fa05be5b669cebe0a2c0e169452119ac
马涛 <1563035429@qq.com>
2023-01-11 15:39:48 +0800

    添加注释

--- src/middleware/article.middleware.js
+++ src/middleware/article.middleware.js
@@ -2,6 +2,7 @@ const Article = require("../model/article.model");
 const { article,global } = require('../constant/responseState')
 
 const articleMiddleware = {
+    // 检查添加、修改文章时的参数
     articleCheck: async ( ctx, next ) => {
         const { title ,content } = ctx.request.body
         if(!title) {
@@ -20,6 +21,7 @@ const articleMiddleware = {
         }
         await next()
     },
+    // 检查修改、查找文章时的id参数
     articleIdCheck: async ( ctx, next ) => {
         const { id } = ctx.request.body
         if(!id) {
